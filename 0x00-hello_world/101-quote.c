34bc648806fe4840727da8e8e7e4b3113addb048
jude iwuji <judeiwuji@gmail.com>
2022-06-02 08:02:23 -0700

    Error fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@ int main(void)
 {
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(msg, 1, sizeof(msg), stderr);
+	fwrite(msg, 1, 59, stderr);
 	return (1);
 }
