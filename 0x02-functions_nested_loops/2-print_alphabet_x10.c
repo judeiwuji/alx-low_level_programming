031c1e442dc4457a4caffb385a842f1d2f83379d
jude iwuji <judeiwuji@gmail.com>
2022-06-07 06:17:31 -0700

    Add: program to print alphabet in lowercase 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase to stdout
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	int alpha;
+	int max;
+
+	while (i < 10)
+	{
+		alpha = 97;
+		max = alpha + 26;
+		while(alpha < max)
+		{
+			_putchar(alpha++);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -4,4 +4,7 @@ This program will print "_putchar", followed by a new line.
 [1-alphabet.c](./1-alphabet.c)  
 This program will print the alphabet, in lowercase, followed by a new line.
 
+[2-print_alphabet_x10.c](./2-print_alphabet_x10.c)
+This program will print 10 times the alphabet, in lowercase, followed by a new line
+
 
