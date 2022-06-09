c9530396ddb41ad9acc146170d73d80ccd0ef151
jude iwuji <judeiwuji@gmail.com>
2022-06-08 23:03:49 -0700

    Add: function to multiply two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - Multiplies two integers
+ * @a: The first interger
+ * @b: The second integer
+ *
+ * Return: The product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -4,4 +4,7 @@ This function will check for uppercase character.
 [1-isdigit.c](./1-isdigit.c)
 This function will check for digit
 
+[2-mul.c](./2-mul.c)
+This function will multiply two intergers
+
 
