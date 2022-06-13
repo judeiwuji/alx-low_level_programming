2cbb252ff8955c5b58625bf544b3d9ed24ab9092
jude iwuji <judeiwuji@gmail.com>
2022-06-13 14:08:29 +0100

    Betty Style

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_rev - This function prints a string, in reverse, followed by a new line.
+ * print_rev - This function prints a string, in reverse, followed
+ * by a new line.
  * @s: the string to be printed
  *
  * Return: void
