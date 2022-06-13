f24acfc89abc5d7fae329421816d31eb7f72fc8e
jude iwuji <judeiwuji@gmail.com>
2022-06-13 20:09:56 +0100

    Fixed failing test

--- 0x05-pointers_arrays_strings/9-main.c
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -11,7 +11,7 @@ int main(void)
 	char s1[98];
 	char *ptr;
 
-	ptr = _strcpy(s1, "");
+	ptr = _strcpy(s1, "Holberton!\n");
 	printf("%s", s1);
 	printf("%s", ptr);
 	return (0);
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,21 +8,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i, len;
-
-	i = 0;
+	int i = 0;
 
 	while (src[i] != '\0')
-		i++;
-
-	len = i + 1;
-	i = 0;
-
-	while (i <= len)
 	{
 		dest[i] = src[i];
 		i++;
 	}
+	dest[i] = '\0';
 
-	return dest;
+	return (dest);
 }
