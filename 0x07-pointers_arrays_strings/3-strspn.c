f9d44ffcd05c07ab17bd8dfc55621b8020f12f2b
jude iwuji <judeiwuji@gmail.com>
2022-06-20 15:59:18 +0100

    Fixed failing case

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,21 +7,25 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int len = 0;
+	unsigned int prev, count;
 	int i, j;
 
-	for (i = 0; accept[i] != '\0'; i++)
+	prev = 0;
+	count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; s[j] != '\0'; j++)
+		prev = count;
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
-				++len;
-				break;
+				++count;
 			}
 		}
+		if (prev == count)
+			break;
 	}
-	if (accept[i] == '\0')
-		len++;
-	return (len);
+
+	return (count);
 }
