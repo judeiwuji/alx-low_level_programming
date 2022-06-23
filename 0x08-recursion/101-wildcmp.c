f79f48d446496ef188b0d1bd6a6dc3eb3effa758
jude iwuji <judeiwuji@gmail.com>
2022-06-23 13:01:55 +0100

    Fixed some test cases.

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * wildcmp - compares two strings and returns 1 if the strings can be
@@ -11,28 +10,23 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	int isWild = *s2 == '*' ? 1 : 0;
-
-	if ((*s1 == '\0') && (*s2 == '\0'))
-		return (1);
-	if ((*s1 == '\0') && !(*s2 + 1 == '\0'))
-		return (0);
-	else if ((*s1 != *s2) && (isWild != 1))
-		return (0);
+	int res;
+	if (*s1 == '\0' && *s2 == '\0')
+		res = 1;
+	else if (*s1 != *s2 && *s2 != '*')
+		res = 0;
+	else if (*s1 == '\0' && *s2 == '*')
+		res = wildcmp(s1, ++s2);
+	else if (*(s1 + 1) == '\0' && *(s2 + 1) != '\0')
+		res = wildcmp(s1, ++s2);
+	else if (*s1 == *s2 && *(s2 + 1) == '*')
+		res = wildcmp(s1, ++s2);
+	else if (*(s1 + 1) != *(s2 + 1) && *(s2 + 1) == '*')
+		res = wildcmp(++s1, ++s2);
+	else if (*(s1 + 1) != *(s2 + 1) && *s2 == '*')
+		res = wildcmp(++s1, s2);
 	else
-	{
-		if (isWild == 1)
-			isWild = (*(s1 + 1) == *(s2 + 1)) ? 0 : 1;
+		res = wildcmp(++s1, ++s2);
 
-		if (isWild == 1)
-		{
-			if (*(s1 + 1) == '\0' && *(s2 + 1) != '\0')
-				++s2;
-			else
-				++s1;
-			return (wildcmp(s1, s2));
-		}
-		else
-			return (wildcmp(++s1, ++s2));
-	}
+	return (res);
 }
