5fe5e71e3672fcdd7b6e41f0d6d5f3a5cd66f6b7
jude iwuji <judeiwuji@gmail.com>
2022-06-27 16:51:59 +0100

    Add: a function that splits a string into words.

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,107 @@
+#include "main.h"
+
+/**
+ * strtow - a function that splits a string into words.
+ * @str: The string to be splited
+ *
+ * Return: double char pointer
+ */
+char **strtow(char *str)
+{
+	char **words = NULL;
+	char *word;
+	int rows, pos, i, m, n;
+
+	if (str != NULL && str[0] != '\0')
+	{
+		rows = _countwords(str);
+		words = (char **)malloc(sizeof(char) * rows + 1);
+
+		pos = 0;
+		m = 0;
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			if (str[i] == ' ')
+			{
+				if (pos > 0)
+				{
+					word = _substr(str, i - pos, i + pos + 1);
+					words[m] = (char *)malloc(sizeof(char) * _strlen(word));
+					if (words[m] != NULL)
+					{
+						for (n = 0; n < pos + 1; n++)
+							words[m][n] = word[n];
+						words[m][n++] = '\0';
+					}
+					m++;
+				}
+				pos = 0;
+				continue;
+			}
+			++pos;
+		}
+		words[m] = NULL;
+	}
+	return (words);
+}
+
+/**
+ * _substr: Will return a string at specified position
+ * @str: The string
+ * @start: The start position
+ * @end: The end position
+ *
+ * Return: char pointer
+ */
+char *_substr(char *str, int start, int end)
+{
+	char *sub;
+	int i;
+
+	sub = malloc(sizeof(char) * (end - start));
+	for (i = 0; start < end; i++, start++)
+		sub[i] = str[start];
+
+	return (sub);
+}
+
+/**
+ * _countwords - count number of words in a string
+ * @str: the string
+ *
+ * Return: int
+ */
+int _countwords(char *str)
+{
+	int count, pos, i;
+
+	pos = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+		{
+			if (pos > 0)
+				++count;
+			pos = 0;
+			continue;
+		}
+		++pos;
+	}
+	return (count);
+}
+
+/**
+ * _strlen - This function returns the length of a string.
+ * @s: The string to return the length
+ *
+ * Return: Length of s
+ */
+int _strlen(char *s)
+{
+	long int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	return (len);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -10,4 +10,7 @@ int _strlen(char *s);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
+char **strtow(char *str);
+char *_substr(char *str, int start, int end);
+int _countwords(char *str);
 #endif
