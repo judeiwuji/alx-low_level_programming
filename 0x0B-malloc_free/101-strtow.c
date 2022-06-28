8749eda99708884095cf467305bc6caa8517f2a3
jude iwuji <judeiwuji@gmail.com>
2022-06-28 14:14:09 +0100

    fixed failing test cases

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,45 +8,45 @@
  */
 char **strtow(char *str)
 {
-	char **words = NULL;
-	char *word;
-	int rows, pos, i, m, n, end;
+char **words = NULL;
+char *word = NULL;
+int row, pos, i, m, n, end;
 
-	if (str != NULL && str[0] != '\0')
-	{
-		rows = _countwords(str);
-		if (rows > 0)
-		{
-			words = (char **)malloc(sizeof(char) * rows + 1);
-			if (words != NULL)
-			{
-				for (i = 0, pos = 0, m = 0; str[i] != '\0'; i++)
-				{
-					if (str[i] == ' ' || str[i + 1] == '\0')
-					{
-						end = str[i + 1] == '\0' ? 1 : 0;
-						if (pos > 0)
-						{
-							word = _substr(str, i - pos, i + end);
-							words[m] = (char *)malloc(sizeof(char) * _strlen(word) + 1);
-							if (words[m] != NULL)
-							{
-								for (n = 0; n < pos + end; n++)
-									words[m][n] = word[n];
-								words[m][n++] = '\0';
-							}
-							m++;
-						}
-						pos = 0;
-						continue;
-					}
-					++pos;
-				}
-				words[m] = NULL;
-			}
-		}
-	}
-	return (words);
+if (str != NULL && str[0] != '\0')
+{
+row = _countwords(str);
+if (row > 0)
+{
+words = (char **)malloc(sizeof(char) * row + 1);
+if (words != NULL)
+{
+for (i = 0, pos = 0, m = 0; str[i] != '\0'; i++)
+{
+if (str[i] == ' ' || str[i + 1] == '\0')
+{
+end = str[i + 1] == '\0' ? 1 : 0;
+if (pos > 0)
+{
+word = _substr(str, i - pos, i + end);
+words[m] = (char *)malloc(sizeof(char) * _strlen(word) + 1);
+if (words[m] != NULL)
+{
+for (n = 0; word[n] != '\0'; n++)
+words[m][n] = (char)word[n];
+words[m][n++] = '\0';
+m++;
+}
+}
+pos = 0;
+continue;
+}
+++pos;
+}
+words[m] = NULL;
+}
+}
+}
+return (words);
 }
 
 /**
@@ -59,16 +59,16 @@ char **strtow(char *str)
  */
 char *_substr(char *str, int start, int end)
 {
-	char *sub;
-	int i, size;
+char *sub;
+int i, size;
 
-	size = end - start;
-	sub = (char *)malloc(sizeof(char) * size + 1);
-	for (i = 0; start < end; i++, start++)
-		sub[i] = str[start];
-	sub[i] = '\0';
+size = end - start;
+sub = (char *)malloc(sizeof(char) * size + 1);
+for (i = 0; start < end; i++, start++)
+sub[i] = str[start];
+sub[i] = '\0';
 
-	return (sub);
+return (sub);
 }
 
 /**
@@ -79,21 +79,21 @@ char *_substr(char *str, int start, int end)
  */
 int _countwords(char *str)
 {
-	int count, pos, i;
+int count, pos, i;
 
-	pos = 0;
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] == ' ')
-		{
-			if (pos > 0)
-				++count;
-			pos = 0;
-			continue;
-		}
-		++pos;
-	}
-	return (count);
+pos = 0;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] == ' ')
+{
+if (pos > 0)
+++count;
+pos = 0;
+continue;
+}
+++pos;
+}
+return (count);
 }
 
 /**
@@ -104,10 +104,10 @@ int _countwords(char *str)
  */
 int _strlen(char *s)
 {
-	long int len = 0;
+long int len = 0;
 
-	while (s[len] != '\0')
-		len++;
+while (s[len] != '\0')
+len++;
 
-	return (len);
+return (len);
 }
