6f7c601dd7cb2dcb07b5a5c42ffca399c7d45362
jude iwuji <judeiwuji@gmail.com>
2022-07-06 13:51:01 +0100

    Fixed failing test case

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include "function_pointers.h"
+
 /**
  * print_name - Prints a name
  * @name: name to be printed
@@ -7,5 +9,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
