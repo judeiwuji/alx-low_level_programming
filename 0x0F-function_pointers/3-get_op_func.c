261d18901047859777ea71a030320a0a8154df4b
jude iwuji <judeiwuji@gmail.com>
2022-07-06 10:45:13 +0100

    Add: a program that performs simple operations.

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects the correct function to perform the
+ * operation asked by the user
+ *
+ * @s: The operator passed as argument to the program
+ * Return: int(*)(int, int)
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}};
+	int i;
+
+	i = 0;
+	while (ops[i].f != NULL)
+	{
+		if (ops[i].op[0] == s[0])
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
