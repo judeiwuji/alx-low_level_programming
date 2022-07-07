201bdb743a3391134c5e43fb957ca45605bd0611
jude iwuji <judeiwuji@gmail.com>
2022-07-07 16:16:36 +0100

    Bug Fix: used string formatter

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		str = va_arg(ap, char *);
 
 		if (str != NULL)
-			printf(str);
+			printf("%s", str);
 		else
 			printf("(nil)");
 
