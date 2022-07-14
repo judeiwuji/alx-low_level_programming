b3bb1e3ed37f015bb803c1eab39f44da8000e247
jude iwuji <judeiwuji@gmail.com>
2022-07-14 14:29:29 +0100

    Add: a function that prints You're beat! and yet, you must allow,\nI bore my house upon my back!\n before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+ * before_main - Executes before main
+ */
+void before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -24,4 +24,5 @@ int _strlen(const char *s);
 char *_strcpy(char *dest, const char *src);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void before_main() __attribute__((constructor));
 #endif
