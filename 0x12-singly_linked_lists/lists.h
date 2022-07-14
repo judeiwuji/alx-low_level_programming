5e83b83c673f26bc5cdb1993a4991f87dd1bd7e2
jude iwuji <judeiwuji@gmail.com>
2022-07-14 14:13:01 +0100

    Add: a function that adds a new node at the end of a list_t list.

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,4 +22,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 int _strlen(const char *s);
 char *_strcpy(char *dest, const char *src);
+list_t *add_node_end(list_t **head, const char *str);
 #endif
