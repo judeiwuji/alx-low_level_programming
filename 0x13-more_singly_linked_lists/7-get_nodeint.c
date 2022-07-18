b0021c1b637319f41b40b8baaa77e3a08fbce016
jude iwuji <judeiwuji@gmail.com>
2022-07-18 15:51:33 +0100

    Add: a function that returns the sum of all the data (n) of a listint_t linked list.

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index -
+ * get_nodeint_at_index - returns the nth node of a listint_t
+ * linked list.
  * @head: The head of the list
  * @index: The index of the node
  *
