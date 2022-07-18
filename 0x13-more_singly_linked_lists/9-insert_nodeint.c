ac0d8335bdd398431e26f9250c2a9ef9eed2c68b
jude iwuji <judeiwuji@gmail.com>
2022-07-18 19:28:41 +0100

    Account for NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *prev = NULL;
 	unsigned int i;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return (NULL);
 	node = *head;
 	for (i = 0; node != NULL; i++)
