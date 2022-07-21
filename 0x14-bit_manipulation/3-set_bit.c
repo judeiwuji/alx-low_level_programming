fe0b37f5feec7e392c4c005a024b31c4ee8203de
jude iwuji <judeiwuji@gmail.com>
2022-07-21 19:27:01 +0100

    Fixed betty style

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,7 +2,7 @@
  * set_bit - sets the value of a bit to 1 at a given index.
  * @n: the digit
  * @index: the index
-
+ *
  * Return: (int)
  */
 int set_bit(unsigned long int *n, unsigned int index)
