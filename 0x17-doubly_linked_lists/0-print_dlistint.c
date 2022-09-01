78d329fb1c5a438ffe1d86de11f333bc276579e7
jude iwuji <judeiwuji@gmail.com>
2022-09-01 02:52:22 -0700

    Fixed betty style

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 
-/* print_dlistint - prints all the elements of a dlistint_t list.
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list.
  * @h: The head of the list
  *
  * Return: The number od nodes in the list
@@ -11,10 +12,9 @@ size_t print_dlistint(const dlistint_t *h)
 	dlistint_t *node;
 	size_t count = 1;
 
-	if(h == NULL)
+	if (h == NULL)
 		return (0);
-
-    printf("%d\n", h->n);
+	printf("%d\n", h->n);
 	node = h->next;
 	while (node != NULL)
 	{
