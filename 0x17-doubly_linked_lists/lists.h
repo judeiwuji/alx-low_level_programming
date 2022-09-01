b1df4f6b5e0a660dc384ec2f5d08d29f3744ff74
jude iwuji <judeiwuji@gmail.com>
2022-09-01 14:13:28 +0100

    Add: Task 3

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -7,7 +7,7 @@
  * @n: integer
  * @prev: points to the previous node
  * @next: points to the next node
- * 
+ *
  * Description: doubly linked list node structure
  *
  */
@@ -23,3 +23,4 @@ typedef struct dlistint_s
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
