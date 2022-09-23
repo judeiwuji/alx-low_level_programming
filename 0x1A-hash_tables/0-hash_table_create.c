551315478b871864c8d6629927ec25f0a6b1c881
jude iwuji <judeiwuji@gmail.com>
2022-09-23 08:08:36 +0100

    Add: Task 0. >>> ht = {}

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hashtable
+ * @size: The size of the hashtable
+ *
+ * Return: pointer
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	return (table);
+}
