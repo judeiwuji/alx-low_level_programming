d452078129b03be0045c57c12ad27cafb46b9a2b
jude iwuji <judeiwuji@gmail.com>
2022-09-23 19:03:53 +0100

    update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,11 +11,15 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node, *node;
+	hash_node_t *new_node;
 
-	if (!ht || !key)
+	if (ht == NULL)
 		return (0);
 	if (ht->size == 0 || ht->array == NULL)
+		return (0);
+	if (key == NULL || key[0] == ' ' || strcmp(key, "") == 0)
+		return (0);
+	if (value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
@@ -24,18 +28,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	new_node->next = NULL;
-	node = ht->array[index];
-	if (node)
-	{
-		new_node->next = node;
-		while (node != NULL)
-		{
-			free(node->value);
-			node->value = strdup(value);
-			node = node->next;
-		}
-	}
+	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
 }
