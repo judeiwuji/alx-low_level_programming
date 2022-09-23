4944a9f15ea096e7af6f277e74411db11cfdacef
jude iwuji <judeiwuji@gmail.com>
2022-09-23 13:42:00 +0100

    update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,6 +15,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL)
 		return (0);
+	if (ht->array == NULL)
+		return (0);
 	if (key == NULL || strcmp(key, "") == 0)
 		return (0);
 	if (value == NULL)
