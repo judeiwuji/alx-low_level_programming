4cfce7a1cd474846eacf962fcd2417aa1d503653
jude iwuji <judeiwuji@gmail.com>
2022-09-23 13:06:14 +0100

    Add: Task 3. >>> ht['betty'] = 'cool'

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: The hash table
+ * @key: The key
+ * @value: The value
+ *
+ * Return: int
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node;
+	hash_node_t *node;
+
+	if (ht == NULL)
+		return (0);
+	if (key == NULL || strcmp(key, "") == 0)
+		return (0);
+
+	index = key_index((const unsigned char *)strdup(key), ht->size);
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+
+	node = ht->array[index];
+	if (node)
+		new_node->next = node;
+	node = new_node;
+	ht->array[index] = node;
+	return (1);
+}
