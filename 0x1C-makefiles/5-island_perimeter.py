ba521d06292e479f5c56b6244f9f830146401a4b
jude iwuji <judeiwuji@gmail.com>
2022-11-09 18:24:13 +0100

    Add: Task 5. Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Contains the `island_perimeter` function."""
+
+def island_perimeter(grid):
+	"""returns the perimeter of the island described in grid"""
+
+	island = []
+
+	for row in grid:
+		square = []
+		for col in row:
+			if col == 1:
+				square.append(col)
+		if len(square) > 0:
+			island.append(square)
+	return 2 * (len(island) + len(island[-1]))
