f591ad9bbe376d0c1045811322d111522bbc8656
jude iwuji <judeiwuji@gmail.com>
2022-11-09 21:49:39 +0100

    update

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -16,8 +16,8 @@ def island_perimeter(grid):
     length = len(island)
     width = len(island[-1])
 
-    if length == width:
-        # is a square island
-        return 4 * len(island)
+    # if length == width:
+    # is a square island
+    # return 4 * len(island)
     # is a rectangle island
     return 2 * (length + width)
